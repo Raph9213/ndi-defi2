4b25b225b02c4f9e1ed86052cdf6e7a13fca2a6b
Thanoux1204 <ehouy92380@gmail.com>
2025-12-04 22:37:36 +0100

    feat: adding frontend project

---
+++ defi2-frontend/app/pages/index.vue
@@ -0,0 +1,21 @@
+<template>
+  <div class="p-10 flex flex-col items-center justify-center min-h-screen gap-4 bg-gray-50 "> <!--dark:bg-gray-900-->
+    <h1 class="text-4xl font-bold text-primary-500">
+      Nuxt UI + Tailwind CSS
+    </h1>
+    <p class="text-gray-600 dark:text-gray-300">
+      Tailwind CSS est déjà inclus avec Nuxt UI !
+    </p>
+    
+    <div class="flex gap-4">
+      <button class="px-4 py-2 text-white bg-green-500 rounded-lg hover:bg-green-600 transition-colors flex items-center gap-2">
+        <span class="i-heroicons-check w-5 h-5" />
+        Bouton Standard
+      </button>
+      
+      <button class="px-4 py-2 text-red-500 bg-blue-500 rounded-lg hover:bg-blue-600 transition-colors">
+        Bouton HTML + Tailwind
+      </button>
+    </div>
+  </div>
+</template>
---
+++ defi2-frontend/tailwind.config.ts
@@ -0,0 +1,11 @@
+import type { Config } from 'tailwindcss'
+
+export default <Partial<Config>>{
+  theme: {
+    extend: {
+      colors: {
+        // Ajoutez vos couleurs personnalisées ici
+      }
+    }
+  }
+}
